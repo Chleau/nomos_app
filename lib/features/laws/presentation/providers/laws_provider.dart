6f419cc2a687a9ba6dcacd16a667f2d761483249
Sooksooksook <theo.inizan@my-digital-school.org>
2025-11-19 14:54:50 +0100

    fix: laws

--- lib/features/laws/presentation/providers/laws_provider.dart
+++ lib/features/laws/presentation/providers/laws_provider.dart
@@ -3,6 +3,7 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:nomos_app/features/laws/data/repositories/law_repository_impl.dart';
 import 'package:nomos_app/features/laws/domain/entities/law.dart';
 import 'package:nomos_app/features/laws/domain/repositories/law_repository.dart';
+import 'package:nomos_app/features/laws/domain/usecases/search_laws_usecase.dart';
 
 final lawRepositoryProvider = Provider<LawRepository>((ref) {
   return LawRepositoryImpl(Supabase.instance.client);
@@ -11,4 +12,16 @@ final lawRepositoryProvider = Provider<LawRepository>((ref) {
 final recentLawsProvider = FutureProvider<List<Law>>((ref) async {
   final repository = ref.watch(lawRepositoryProvider);
   return repository.getRecentLaws();
+});
+
+final searchLawsUseCaseProvider = Provider<SearchLaws>((ref) {
+  return SearchLaws(ref.watch(lawRepositoryProvider));
+});
+
+final searchQueryProvider = StateProvider<String>((ref) => '');
+
+final filteredLawsProvider = FutureProvider<List<Law>>((ref) async {
+  final searchUseCase = ref.watch(searchLawsUseCaseProvider);
+  final query = ref.watch(searchQueryProvider);
+  return searchUseCase(query);
 });
