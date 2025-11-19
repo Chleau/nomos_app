526432db642214fd049c27e96814f75c36353017
Sooksooksook <theo.inizan@my-digital-school.org>
2025-11-19 16:01:25 +0100

    add: navbar

--- lib/app.dart
+++ lib/app.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'features/auth/presentation/widgets/auth_guard.dart';
 import 'features/home/presentation/pages/home_page.dart';
 import 'features/laws/presentation/pages/laws_page.dart';
+import 'shared/widgets/Navbar.dart';
 
 
 class MyApp extends StatelessWidget {
@@ -35,9 +36,45 @@ class MyApp extends StatelessWidget {
           useMaterial3: true,
         ),
         home: const AuthGuard(
-          child: HomePage(),
+          child: MainPage(),
         ),
         debugShowCheckedModeBanner: false,
+      ),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = [
+    const HomePage(),
+    const Center(child: Text('Carte incidents', style: TextStyle(fontSize: 24))),
+    const LawsPage(),
+    const Center(child: Text('Menu', style: TextStyle(fontSize: 24))),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: _currentIndex,
+        children: _pages,
+      ),
+      bottomNavigationBar: Navbar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
       ),
     );
   }
